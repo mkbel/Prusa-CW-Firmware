1a453e97b44c003dbda5591a4e9bbb00d375dcad
Marek Bel <marek.bel@posta.cz>
2019-10-10 19:00:32 +0200

    There is no difference in memory usage if messages are not defined in separate file.

--- Prusa-CW-Firmware/main.cpp
+++ Prusa-CW-Firmware/main.cpp
@@ -12,7 +12,6 @@
 #include "PrusaLcd.h"
 #include "MenuList.h"
 #include "SpeedControl.h"
-#include "messages.h"
 //#include "LiquidCrystal_Prusa.h"
 
 using Ter = PrusaLcd::Terminator;
@@ -20,6 +19,62 @@ using Ter = PrusaLcd::Terminator;
 #define EEPROM_OFFSET 128
 #define MAGIC_SIZE 6
 //#define SERIAL_COM_DEBUG	//!< Set up for communication through USB
+
+#define FW_VERSION  "2.1.4"
+
+static const char pgmstr_back[] PROGMEM = "Back";
+static const char pgmstr_fan1_curing[] PROGMEM = "FAN1 curing";
+static const char pgmstr_fan1_drying[] PROGMEM = "FAN1 drying";
+static const char pgmstr_fan2_curing[] PROGMEM = "FAN2 curing";
+static const char pgmstr_fan2_drying[] PROGMEM = "FAN2 drying";
+static const char pgmstr_curing[] PROGMEM = "Curing";
+static const char pgmstr_drying[] PROGMEM = "Drying";
+static const char pgmstr_washing[] PROGMEM = "Washing";
+static const char pgmstr_resin_preheat[] PROGMEM = "Resin preheat";
+static const char pgmstr_rotation_speed[] PROGMEM = "Rotation speed";
+static const char pgmstr_run_mode[] PROGMEM = "Run mode";
+static const char pgmstr_preheat[] PROGMEM = "Preheat";
+static const char pgmstr_sound[] PROGMEM = "Sound";
+static const char pgmstr_fans[] PROGMEM = "Fans";
+static const char pgmstr_led_intensity[] PROGMEM = "LED intensity";
+static const char pgmstr_information_error[] PROGMEM = "Information ->!!";
+static const char pgmstr_information[] PROGMEM = "Information";
+static const char pgmstr_unit_system[] PROGMEM = "Unit system";
+static const char pgmstr_fw_version[] PROGMEM = "FW version: "  FW_VERSION;
+static const char pgmstr_fan1_failure[] PROGMEM = "FAN1 failure";
+static const char pgmstr_fan2_failure[] PROGMEM = "FAN2 failure";
+static const char pgmstr_heater_failure[] PROGMEM = "HEATER failure";
+static const char *pgmstr_serial_number = reinterpret_cast<const char *>(0x7fe0);
+static const char pgmstr_build_nr[] PROGMEM = "Build: " FW_BUILDNR;
+static const char pgmstr_fw_hash[] PROGMEM = FW_HASH;
+static const char pgmstr_workspace[] PROGMEM = FW_LOCAL_CHANGES ? "Workspace dirty" : "Workspace clean";
+
+static const __FlashStringHelper *str_back = reinterpret_cast<const __FlashStringHelper*>(pgmstr_back);
+static const __FlashStringHelper *str_fan1_curing = reinterpret_cast<const __FlashStringHelper*>(pgmstr_fan1_curing);
+static const __FlashStringHelper *str_fan1_drying = reinterpret_cast<const __FlashStringHelper*>(pgmstr_fan1_drying);
+static const __FlashStringHelper *str_fan2_curing = reinterpret_cast<const __FlashStringHelper*>(pgmstr_fan2_curing);
+static const __FlashStringHelper *str_fan2_drying = reinterpret_cast<const __FlashStringHelper*>(pgmstr_fan2_drying);
+static const __FlashStringHelper *str_curing = reinterpret_cast<const __FlashStringHelper*>(pgmstr_curing);
+static const __FlashStringHelper *str_drying = reinterpret_cast<const __FlashStringHelper*>(pgmstr_drying);
+static const __FlashStringHelper *str_washing = reinterpret_cast<const __FlashStringHelper*>(pgmstr_washing);
+static const __FlashStringHelper *str_resin_preheat = reinterpret_cast<const __FlashStringHelper*>(pgmstr_resin_preheat);
+static const __FlashStringHelper *str_rotation_speed = reinterpret_cast<const __FlashStringHelper*>(pgmstr_rotation_speed);
+static const __FlashStringHelper *str_run_mode = reinterpret_cast<const __FlashStringHelper*>(pgmstr_run_mode);
+static const __FlashStringHelper *str_preheat = reinterpret_cast<const __FlashStringHelper*>(pgmstr_preheat);
+static const __FlashStringHelper *str_sound = reinterpret_cast<const __FlashStringHelper*>(pgmstr_sound);
+static const __FlashStringHelper *str_fans = reinterpret_cast<const __FlashStringHelper*>(pgmstr_fans);
+static const __FlashStringHelper *str_led_intensity = reinterpret_cast<const __FlashStringHelper*>(pgmstr_led_intensity);
+static const __FlashStringHelper *str_information_error = reinterpret_cast<const __FlashStringHelper*>(pgmstr_information_error);
+static const __FlashStringHelper *str_information = reinterpret_cast<const __FlashStringHelper*>(pgmstr_information);
+static const __FlashStringHelper *str_unit_system = reinterpret_cast<const __FlashStringHelper*>(pgmstr_unit_system);
+static const __FlashStringHelper *str_fw_version = reinterpret_cast<const __FlashStringHelper*>(pgmstr_fw_version);
+static const __FlashStringHelper *str_fan1_failure = reinterpret_cast<const __FlashStringHelper*>(pgmstr_fan1_failure);
+static const __FlashStringHelper *str_fan2_failure = reinterpret_cast<const __FlashStringHelper*>(pgmstr_fan2_failure);
+static const __FlashStringHelper *str_heater_failure = reinterpret_cast<const __FlashStringHelper*>(pgmstr_heater_failure);
+static const __FlashStringHelper *str_serial_number = reinterpret_cast<const __FlashStringHelper*>(pgmstr_serial_number);
+static const __FlashStringHelper *str_build_nr = reinterpret_cast<const __FlashStringHelper*>(pgmstr_build_nr);
+static const __FlashStringHelper *str_fw_hash = reinterpret_cast<const __FlashStringHelper*>(pgmstr_fw_hash);
+static const __FlashStringHelper *str_workspace = reinterpret_cast<const __FlashStringHelper*>(pgmstr_workspace);
 
 typedef char Serial_num_t[20]; //!< Null terminated string for serial number
 
